5db4297795a7f91bc53be8534c6a2eb095bf9a5d
jiseon_an <ajsajs1031@gmail.com>
2024-12-05 16:25:58 +0900

    TS DAY1

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const nums = 2024;
+const temperature = 7;
+const str = "TypeScript";
+const isTrue = true;
+const something = null;
+let someValue;
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+const nums: number = 2024;
+const temperature: number = 7;
+
+const str: string = "TypeScript";
+
+const isTrue: boolean = true;
+
+const something: string | null = null;
+
+let someValue: any;
